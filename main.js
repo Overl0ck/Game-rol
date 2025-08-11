85a90070d448126bbed4bb82200fb1041374310f
Overl0ck <manuelangelbravo.mab@gmail.com>
2025-08-11 12:06:31 +0200

    ELECTRON

---
+++ main.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 720,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  // Cargar tu juego HTML
+  win.loadFile(path.join(__dirname, 'index.html'));
+
+  // Opcional: abrir herramientas de desarrollo
+  // win.webContents.openDevTools();
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
